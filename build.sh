5a002dbcb5e0f8eab6e1eb567d60227e367a6f26
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2023-09-27 14:10:06 +0300

    yay

---
+++ build.sh
@@ -0,0 +1,6 @@
+mkdir ./build
+
+cd ./build/
+cmake .. && cmake --build .
+
+echo 'done compiling'
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "imgui.h"
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+}
