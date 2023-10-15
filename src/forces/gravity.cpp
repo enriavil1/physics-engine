4550fa0589b37e99c585c483948915cd65d78d2a
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2023-10-15 16:26:06 +0300

    wow

--- src/forces/gravity.cpp
+++ src/forces/gravity.cpp
@@ -1,10 +1,11 @@
 #include "./gravity.hpp"
 
-ImVec2 Gravity::apply(float mass, float current_velocity) {
-  float drag_force = 0.5 * Gravity::drag_coeffiecient *
-                     (current_velocity *
-                      current_velocity); // D = 0.5 * (rho * C * Area * vel^2)
-  ImVec2 drag_acc = ImVec2(0, drag_force / mass);
-
-  return ImVec2(0, (Gravity::gravity_acc.y - drag_acc.y));
-}
+// ImVec2 Gravity::apply(float mass, float current_velocity) {
+//   float drag_force = 0.5 * Gravity::drag_coeffiecient *
+//                      (current_velocity *
+//                       current_velocity); // D = 0.5 * (rho * C * Area *
+//                       vel^2)
+//   ImVec2 drag_acc = ImVec2(0, drag_force / mass);
+//
+//   return ImVec2(0, (Gravity::gravity_acc.y - drag_acc.y));
+// }
