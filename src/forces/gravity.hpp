9c22df5a92f220deaa3fb2dfd6b9b68b65fa9528
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2023-10-15 16:33:56 +0300

    ya

--- src/forces/gravity.hpp
+++ src/forces/gravity.hpp
@@ -8,12 +8,5 @@ private:
   static constexpr float drag_coeffiecient = 0.5f;
 
 public:
-  ImVec2 apply(float mass, float current_velocity) {
-    float drag_force = 0.5 * Gravity::drag_coeffiecient *
-                       (current_velocity *
-                        current_velocity); // D = 0.5 * (rho * C * Area * vel^2)
-    ImVec2 drag_acc = ImVec2(0, drag_force / mass);
-
-    return ImVec2(0, (Gravity::gravity_acc.y - drag_acc.y));
-  };
+  ImVec2 apply(float mass, float current_velocity) override;
 };
