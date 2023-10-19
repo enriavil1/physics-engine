b0bfd683d76fcf9e3856b2b599a2b85e9a346176
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2023-10-19 20:19:30 +0300

    added so cells take double the size of the biggest object

--- src/physics/grid/grid.hpp
+++ src/physics/grid/grid.hpp
@@ -12,8 +12,8 @@ class Grid {
 private:
   // TODO (enriavil1): add variable width and height cells by taking the biggest
   // size of width and height from objects
-  const uint32_t m_width = 20;
-  const uint32_t m_height = 20;
+  uint32_t m_width = 20;
+  uint32_t m_height = 20;
 
   std::unordered_map<uint32_t, GridCell> m_cells =
       std::unordered_map<uint32_t, GridCell>{};
@@ -21,10 +21,15 @@ private:
 public:
   void clear() { this->m_cells = std::unordered_map<uint32_t, GridCell>{}; }
 
-  uint32_t getWidth() { return this->m_width; }
-  uint32_t getHeight() { return this->m_height; }
+  void updateWidthAndHeight(uint32_t width, uint32_t height) {
+    this->m_width = width;
+    this->m_height = height;
+  }
 
-  uint32_t getCellID(uint32_t pos_x, uint32_t pos_y) {
+  uint32_t getWidth() const { return this->m_width; }
+  uint32_t getHeight() const { return this->m_height; }
+
+  uint32_t getCellID(uint32_t pos_x, uint32_t pos_y) const {
     const auto window_width = ImGui::GetWindowWidth();
     const auto window_height = ImGui::GetWindowHeight();
     const auto window_pos = ImGui::GetWindowPos();
