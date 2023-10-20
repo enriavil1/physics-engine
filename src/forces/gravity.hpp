d00fd7886f7b4288aa92e5e92ca6944a4db65902
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2023-10-20 16:43:59 +0300

    drag in gravity should only apply when you are moving downwards

--- src/forces/gravity.hpp
+++ src/forces/gravity.hpp
@@ -10,10 +10,15 @@ private:
 public:
   ImVec2 apply(const float& mass, const float& current_velocity,
                const float& area) {
-    float drag_force = 0.5f * Gravity::drag_coeffiecient *
-                       (current_velocity *
-                        current_velocity); // D = 0.5 * (rho * C * Area * vel^2)
-    ImVec2 drag_acc = ImVec2(0, drag_force / mass);
+
+    ImVec2 drag_acc;
+    if (current_velocity > 0) {
+      float drag_force =
+          0.5f * Gravity::drag_coeffiecient *
+          (current_velocity *
+           current_velocity); // D = 0.5 * (rho * C * Area * vel^2)
+      drag_acc = ImVec2(0, drag_force / mass);
+    }
 
     return ImVec2(0, (Gravity::gravity_acc.y - drag_acc.y));
   };
