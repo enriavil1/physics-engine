7086a38dd5b71894b3c5e92080b262ea056365d4
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2023-10-24 17:24:52 +0300

    sad

--- src/thread_pool/worker.hpp
+++ src/thread_pool/worker.hpp
@@ -16,8 +16,26 @@ private:
   TaskQueue *m_task_queue = nullptr;
 
 public:
-  Worker(TaskQueue& task_queue, uint32_t id);
+  Worker(TaskQueue& task_queue, uint32_t id)
+      : m_id{id}, m_task_queue{&task_queue} {
+    this->m_thread = std::thread([this]() { this->run(); });
+  }
 
-  void run();
-  void stop();
+  void run() {
+    while (this->m_is_running) {
+      this->m_task_queue->getTask(this->m_task);
+      if (this->m_task == nullptr) {
+        // there arent any task in queue so we wait for a task
+        TaskQueue::wait();
+      } else {
+        this->m_task();
+        this->m_task = nullptr;
+      }
+    }
+  }
+
+  void stop() {
+    this->m_is_running = false;
+    this->m_thread.join();
+  }
 };
